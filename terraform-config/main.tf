0d1006b28eb4636853b717f9e73600c5fa623396
jbergknoff <jonathan@bergknoff.com>
2019-03-21 15:03:18 -0500

    Bump to node 8

--- terraform-config/main.tf
+++ terraform-config/main.tf
@@ -57,7 +57,7 @@ resource "aws_lambda_function" "alexa_guitar_tuner" {
   function_name = "alexa-guitar-tuner"
   role = "${aws_iam_role.lambda_role.arn}"
   handler = "index.handler"
-  runtime = "nodejs6.10"
+  runtime = "nodejs8.10"
   filename = "${var.zip_filename}"
   source_code_hash = "${base64sha256(file(var.zip_filename))}"
 }
